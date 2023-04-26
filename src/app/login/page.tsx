197b86238f03c0daa5d248e87a8afa91c65511bf
vitornegre <vitornegresiolo@gmail.com>
2023-04-26 18:56:33 -0300

    fix: title & font

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import { Container, ContainerCardContent } from '../components/Container';
 import { CardGray, CardWhite } from '../components/Card';
+import { Title } from '../components/Title';
 
 import logo from 'public/images/logo.svg';
 import Image from 'next/image';
@@ -11,12 +12,12 @@ const hind = Hind({ subsets: ['latin'], weight: '700'})
 
 export default function LoginPage() {
     return (
-        <Container>
+        <Container className={hind.className}>
            <CardGray>
                 <CardWhite>
                    <ContainerCardContent>
                     <Image src={logo} alt='logo_note_maua' width={250} />
-                    <h2>Login</h2>
+                    <Title>Login</Title>
                    </ContainerCardContent>
                 </CardWhite>
            </CardGray>
