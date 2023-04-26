e48295ca621f8b455049733fa09770b1788596a0
Gabriel Parmigiano <parmigianogabriel@gmail.com>
2023-04-26 16:17:06 -0300

    Started Login Page

---
+++ src/app/components/Container.tsx
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+    width: 100%;
+    height: 100%;
+    background-color: #B2DAFF;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+`;
+
+const ContainerCardContent = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    padding-top: 16px;
+    
+`;
+
+export { Container, ContainerCardContent };
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,1 +1,25 @@
 'use client';
+import React from 'react';
+import { Container, ContainerCardContent } from '../components/Container';
+import { CardGray, CardWhite } from '../components/Card';
+
+import logo from 'public/images/logo.svg';
+import Image from 'next/image';
+
+import { Hind } from 'next/font/google';
+const hind = Hind({ subsets: ['latin'], weight: '700'})
+
+export default function LoginPage() {
+    return (
+        <Container>
+           <CardGray>
+                <CardWhite>
+                   <ContainerCardContent>
+                    <Image src={logo} alt='logo_note_maua' width={250} />
+                    <h2>Login</h2>
+                   </ContainerCardContent>
+                </CardWhite>
+           </CardGray>
+        </Container>
+    )
+}
