6c7b8cd86d13f29ea7c8a0e2d7f6fc2786f57a9a
vitornegre <vitornegresiolo@gmail.com>
2023-04-27 15:50:12 -0300

    tela de definir senha

---
+++ src/app/definir-senha/page.tsx
@@ -0,0 +1,53 @@
+'use client';
+import React from 'react';
+
+
+import {Container, ContainerCardContent, ContainerRowLink} from '../components/Container';
+import {CardGray, CardWhite} from '../components/Card';
+
+import { Title } from '../components/Title';
+
+import { useForm, SubmitHandler } from "react-hook-form";
+
+import {Hind} from 'next/font/google';
+const hind = Hind({subsets: ['latin'], weight: '700'})
+
+import { FormButton, FormContainer, FormInput, FormLabel } from '../components/Form';
+import ImageComponentMaua from '../components/ImageComponent/LogoMaua';
+import ImageComponentNoteMaua from '../components/ImageComponent/LogoNoteMaua';
+
+
+export interface IFormlogin {
+  email: string;
+  password: string;
+}
+
+export default function LoginPage() {
+
+    const {register, handleSubmit } = useForm<IFormlogin>();
+
+    const onSubmit: SubmitHandler<IFormlogin> = data => {};
+
+    return (
+      <Container className={hind.className}>
+        <CardGray>
+          <CardWhite>
+              <ContainerCardContent>
+                <ImageComponentNoteMaua/>
+                <Title>Definir Senha</Title>
+                <FormContainer onSubmit={handleSubmit(onSubmit)}>
+                  <FormLabel htmlFor='password'>Senha</FormLabel>
+                  <FormInput type='password' {...register('password', { required: true })}/>
+                  <FormLabel style={{paddingRight: ''}} htmlFor='password'>Confirme a Senha</FormLabel>
+                  <FormInput type='password' {...register('password', { required: true})} />
+                  <FormButton type='submit'>Definir</FormButton>
+                </FormContainer>
+              </ContainerCardContent>
+          </CardWhite>
+        </CardGray>
+          <ImageComponentMaua />
+      </Container>
+    )
+}
+
+
