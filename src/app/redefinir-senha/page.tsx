0c5fcd77d9971872dac748c43433525535ed8a4e
vitornegre <vitornegresiolo@gmail.com>
2023-05-02 13:23:29 -0300

    tela redefinir senha

---
+++ src/app/redefinir-senha/page.tsx
@@ -0,0 +1,52 @@
+'use client';
+import React from 'react';
+
+
+import {Container, ContainerCardContent} from '../components/Container';
+import {CardGray, CardWhite} from '../components/Card';
+
+import { Title } from '../components/Title';
+
+import { useForm, SubmitHandler } from "react-hook-form";
+
+import {Hind} from 'next/font/google';
+const hind = Hind({subsets: ['latin'], weight: '700'})
+
+import { FormButton, FormContainer, FormInput, FormLabel } from '../components/Form';
+import ImageComponentMaua from '../components/ImageComponent/LogoMaua';
+import ImageComponentNoteMaua from '../components/ImageComponent/LogoNoteMaua';
+
+
+export interface IFormRedefinirSenha {
+  password: string;
+}
+
+export default function DefinirSenhaPage() {
+
+    const {register, handleSubmit } = useForm<IFormRedefinirSenha>();
+
+    const onSubmit: SubmitHandler<IFormRedefinirSenha> = data => {};
+
+    return (
+      <Container className={hind.className}>
+        <CardGray>
+          <CardWhite>
+              <ContainerCardContent>
+                <ImageComponentNoteMaua/>
+                <Title>Redefinir Senha</Title>
+                <FormContainer onSubmit={handleSubmit(onSubmit)}>
+                  <FormLabel htmlFor='password'>Nova senha</FormLabel>
+                  <FormInput type='password' {...register('password', { required: true })}/>
+                  <FormLabel style={{paddingRight: ''}} htmlFor='password'>Confirme a Senha</FormLabel>
+                  <FormInput type='password' {...register('password', { required: true})} />
+                  <FormButton style={{marginTop:'46px'}}type='submit'>Confirmar</FormButton>
+                </FormContainer>
+              </ContainerCardContent>
+          </CardWhite>
+        </CardGray>
+          <ImageComponentMaua />
+      </Container>
+    )
+}
+
+
