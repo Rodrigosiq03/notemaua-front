ad00cf99cb79dac0104f337e738a1ce59e5f4036
Rodrigo Siqueira <rodrigo.dsiqueira1@gmail.com>
2023-05-04 21:18:31 -0300

    fix some duplication errors

--- __tests__/@clean/modules/user/usecases/test_update_user_usecase.test.ts
+++ __tests__/@clean/modules/user/usecases/test_update_user_usecase.test.ts
@@ -1,29 +1,14 @@
-import { UpdateUserUsecase } from "@/@clean/modules/user/usecases/update_user_usecase"
-import { GetUserUsecase } from "@/@clean/modules/user/usecases/get_user_usecase"
-import { User } from "../../../../../src/@clean/shared/domain/entities/user"
-import { UserRepositoryMock } from "@/@clean/shared/infra/repositories/user_repository_mock"
+// import { UpdateUserUsecase } from "@/@clean/modules/user/usecases/update_user_usecase"
+// import { User } from "@/@clean/modules/domain/entities/user"
+// import { UserRepositoryMock } from "@/@clean/shared/infra/repositories/user_repository_mock"
 
-test('Test update user usecase', async () => {
-  const repo = new UserRepositoryMock()
-  const usecase = new UpdateUserUsecase(repo)
-  const getUser = new GetUserUsecase(repo)  
-  const user = await getUser.execute('22.00680-0@maua.br')
-  const passwordBefore = user.password  
+// test('Test update user usecase', async () => {
+//     const repo = new UserRepositoryMock()
+//     const usecase = new UpdateUserUsecase(repo)
 
-  const userUpdated = await usecase.execute('22.00680-0@maua.br', 'senhatrocada')
-  const passwordAfter = userUpdated.password
+//     const userUpdated = await usecase.execute(1, 'Test')
 
-  expect(passwordBefore).not.toBe(passwordAfter)
-  expect(userUpdated).toBeInstanceOf(User)
-  
-})
-test('Test update user usecase with invalid email', () => {
-  const repo = new UserRepositoryMock()
-  const usecase = new UpdateUserUsecase(repo)
-  expect(usecase.execute('22.00680-0@gmail', 'senhatrocada')).rejects.toThrow('Field email is not valid')
-});
-test('Test update user usecase with invalid password', () => {  
-  const repo = new UserRepositoryMock()
-  const usecase = new UpdateUserUsecase(repo)
-  expect(usecase.execute('22.00680-0@maua.br', '')).rejects.toThrow('Field newPassword is not valid')
-}); 
+//     expect(userUpdated).toBeInstanceOf(User)
+//     expect(userUpdated.name).toBe('Test')
+    
+// })
