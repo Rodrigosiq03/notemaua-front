3c9bae3c66f26ab9c11e8ce73f4a28f98cd12ef9
vitornegre <vitornegresiolo@gmail.com>
2023-05-17 10:50:11 -0300

    inicio pop-up terms

--- src/app/retirada-usuario/page.tsx
+++ src/app/retirada-usuario/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { CardGray, CardWhite } from "../components/Card";
 import { Container, ContainerCardContent, ContainerRow, ContainerRowLink } from "../components/Container";
-import { FormButton, FormButtonScan, FormContainer, FormInput, FormLabel } from "../components/Form";
+import { FormButton, FormButtonScan, FormButtonTerms, FormContainer, FormInput, FormLabel } from "../components/Form";
 import { ReturnIcon, ScanIcon } from "../components/Icon";
 import ImageComponentMaua from "../components/ImageComponent/LogoMaua";
 import ImageComponentNoteMaua from "../components/ImageComponent/LogoNoteMaua";
@@ -11,13 +11,30 @@ import { Title } from "../components/Title";
 import { Hind } from 'next/font/google';
 const hind = Hind({ subsets: ['latin'], weight: ['700', '300'] });
 
+
+import DialogComponentTermsOfUse from '../components/DialogMUI/DialogTermsOfUse';
+import React from "react";
+import { DialogProps } from "@mui/material";
+
 export interface IFormRetirada{
   numSerie: string;
 }
 
-
-
 export default function RetiradaPage(){
+
+  // logic dialog for terms of use
+
+  const [open, setOpen] = React.useState(false);
+  
+    const handleClickOpenDialogTermsOfUse = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+  
+
   return(
     <Container className={hind.className}>
       <CardGray>
@@ -34,7 +51,7 @@ export default function RetiradaPage(){
               </FormButtonScan>
               <ContainerRowLink style={{paddingTop: '34px'}}>
                   <TextForLink style={{fontWeight: '300'}}><input type="checkbox"/>Concordo com os</TextForLink>
-                  <LinkStyled href="/termosDeUso">termos de uso.</LinkStyled>
+                  <FormButtonTerms onClick={handleClickOpenDialogTermsOfUse}>termos de uso.</FormButtonTerms>
               </ContainerRowLink>
               <FormButton type="submit" style={{backgroundColor: '#00CE3A'}}>Confirmar</FormButton>
             </FormContainer>
@@ -46,6 +63,12 @@ export default function RetiradaPage(){
         </CardWhite>
       </CardGray>
       <ImageComponentMaua/>
+      <DialogComponentTermsOfUse
+        open={open}
+        handleClose={handleClose}     
+      >
+      teste
+      </DialogComponentTermsOfUse>
     </Container>
   )
 }
