cde9e8bfbca0cd535cd744973cbd96f7bdd0defc
vitornegre <vitornegresiolo@gmail.com>
2023-05-17 21:25:58 -0300

    ajuste texto termos de uso

--- src/app/retirada-usuario/page.tsx
+++ src/app/retirada-usuario/page.tsx
@@ -70,14 +70,14 @@ export default function RetiradaPage(){
       <p>Declaro que estou ciente de que Centro Universitário do Instituto Mauá de Tecnologia (IMT) cederá um kit notebook para o meu uso nas aulas práticas de computação quando ocorrerem em que não sejam laboratórios de informática equipados com desktops.
 Conforme horário de aulas e indicação de uso, poderei retirar com um funcionário do corpo técnico do IMT um kit notebook, composto: de 1 (um) notebook, 1 (uma) fonte e 1 (um) mouse.</p>
       <p><strong>Para esse uso, assumo as responsabilidades abaixo e estou ciente de que:</strong><br/><br/>
-      A retirada do kit notebook se dará pela minha presença nas salas U20 ou H207, conforme o prédio onde se dará as aulas, onde haverá um mecanismo de identificação e de controle de retirada;
-o controle de retirada e uso se dará pelo número de série do notebook, horário de retirada e pela minha assinatura ou outra forma de autenticação;<br/>
-      Ao final de cada aula prática, me comprometo a devolver o kit notebook no mesmo local onde foi retirado, e assinarei um protocolo de entrega, com o horário da devolução;<br/>
-      Estou ciente de que poderei ficar com o kit notebook retirado, caso a aula seguinte também seja uma aula prática e não for ser ministrada em sala onde já haja desktops;<br/>
-      Não devo circular com o kit notebook nas dependências da Mauá;<br/>
-      Não devo sair em hipótese alguma do campus da Mauá com o kit notebook;<br/>
-      É de minha responsabilidade zelar pela guarda e conservação do kit notebook que me foi cedido para uso;<br/>
-      Caso ocorra algo com o kit notebook ou algum problema de funcionamento, comunicarei ao técnico responsável na sala em que retirei o kit notebook;
+      - A retirada do kit notebook se dará pela minha presença nas salas U20 ou H207, conforme o prédio onde se dará as aulas, onde haverá um mecanismo de identificação e de controle de retirada;
+o controle de retirada e uso se dará pelo número de série do notebook, horário de retirada e pela minha assinatura ou outra forma de autenticação;<br/><br/>
+      - Ao final de cada aula prática, me comprometo a devolver o kit notebook no mesmo local onde foi retirado, e assinarei um protocolo de entrega, com o horário da devolução;<br/><br/>
+      - Estou ciente de que poderei ficar com o kit notebook retirado, caso a aula seguinte também seja uma aula prática e não for ser ministrada em sala onde já haja desktops;<br/><br/>
+      - Não devo circular com o kit notebook nas dependências da Mauá;<br/><br/>
+      - Não devo sair em hipótese alguma do campus da Mauá com o kit notebook;<br/><br/>
+      - É de minha responsabilidade zelar pela guarda e conservação do kit notebook que me foi cedido para uso;<br/><br/>
+      - Caso ocorra algo com o kit notebook ou algum problema de funcionamento, comunicarei ao técnico responsável na sala em que retirei o kit notebook;
       </p>
       </DialogComponentTermsOfUse>
     </Container>
