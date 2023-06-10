8bc646084f6f3fdf73148b0e663f0030a82c4ed9
Rodrigo Siqueira <rodrigo.dsiqueira1@gmail.com>
2023-06-10 11:28:00 -0300

    Revert "commented tests"
    This reverts commit 5035fed9898de41a76e81824d9cd9a7e838dc4de.

--- __tests__/@clean/modules/withdraw/usecases/test_create_withdraw_usecase.test.ts
+++ __tests__/@clean/modules/withdraw/usecases/test_create_withdraw_usecase.test.ts
@@ -2,7 +2,6 @@ import { CreateWithdrawUsecase } from '../../../../../src/@clean/modules/withdra
 import { Withdraw } from '../../../../../src/@clean/shared/domain/entities/withdraw';
 import { WithdrawRepositoryMock } from '../../../../../src/@clean/shared/infra/repositories/withdraw_repository_mock';
 
-<<<<<<< Updated upstream
 test('Test create withdraw usecase', async () => {
   const repo = new WithdrawRepositoryMock();
   const usecase = new CreateWithdrawUsecase(repo);
@@ -14,25 +13,3 @@ test('Test create withdraw usecase with invalid numSerie', () => {
   const usecase = new CreateWithdrawUsecase(repo);
   expect(usecase.execute('')).rejects.toThrow('Field numSerie is not valid');
 });
-=======
-// test('Test create withdraw usecase', async () => {
-//   const repo = new WithdrawRepositoryMock();
-//   const usecase = new CreateWithdrawUsecase(repo);
-//   const withdraw = await usecase.execute('34100', '22.00680-0@maua.br');
-//   expect(withdraw).toBeInstanceOf(Withdraw);
-// });
-// test('Test create withdraw usecase with invalid numSerie', () => {
-//   const repo = new WithdrawRepositoryMock();
-//   const usecase = new CreateWithdrawUsecase(repo);
-//   expect(usecase.execute('', '22.00680-0@maua.br')).rejects.toThrow(
-//     'Field numSerie is not valid'
-//   );
-// });
-// test('Test create withdraw usecase with invalid email', () => {
-//   const repo = new WithdrawRepositoryMock();
-//   const usecase = new CreateWithdrawUsecase(repo);
-//   expect(usecase.execute('34100', '22.00680-0@gmail.com')).rejects.toThrow(
-//     'Field email is not valid'
-//   );
-// });
->>>>>>> Stashed changes
--- __tests__/@clean/modules/withdraw/usecases/test_finish_withdraw_usecase.test.ts
+++ __tests__/@clean/modules/withdraw/usecases/test_finish_withdraw_usecase.test.ts
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream
 import { FinishWithdrawUsecase } from '../../../../../src/@clean/modules/withdraw/usecases/finish_withdraw_usecase';
 import { Withdraw } from '../../../../../src/@clean/shared/domain/entities/withdraw';
 import { WithdrawRepositoryMock } from '../../../../../src/@clean/shared/infra/repositories/withdraw_repository_mock';
@@ -14,20 +13,3 @@ test('Test finish withdraw usecase with invalid numSerie', () => {
   const usecase = new FinishWithdrawUsecase(repo);
   expect(usecase.execute('')).rejects.toThrow('Field numSerie is not valid');
 });
-=======
-// import { FinishWithdrawUsecase } from '../../../../../src/@clean/modules/withdraw/usecases/finish_withdraw_usecase';
-// import { Withdraw } from '../../../../../src/@clean/shared/domain/entities/withdraw';
-// import { WithdrawRepositoryMock } from '../../../../../src/@clean/shared/infra/repositories/withdraw_repository_mock';
-
-// // test('Test finish withdraw usecase', () => {
-// //   const repo = new WithdrawRepositoryMock();
-// //   const usecase = new FinishWithdrawUsecase(repo);
-
-// //   expect(usecase.execute('34100')).toBeInstanceOf(Promise<Withdraw>);
-// // });
-// // test('Test finish withdraw usecase with invalid numSerie', () => {
-// //   const repo = new WithdrawRepositoryMock();
-// //   const usecase = new FinishWithdrawUsecase(repo);
-// //   expect(usecase.execute('')).rejects.toThrow('Field numSerie is not valid');
-// // });
->>>>>>> Stashed changes
